9b9ac97b7b543c5cc99ecb69f53eba0647a80487
Hinoronori Nakano <noribasket0429200001@yahoo.co.jp>
2020-11-16 00:32:45 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Todo from './todo.js'
+
+// class MainPage extends React.Component {
+	// render(){
+		// return(
+			// <div className='MainPage'>
+				// <h1>Hironori Nakano</h1>
+				// <p>Yahoo Enginner</p>
+			// </div>
+		// )
+	// }
+// }
+
+// ReactDOM.render(<MainPage />,document.getElementById('root'))
+ReactDOM.render(<Todo />, document.getElementById('root'));
