56e0a25537416e019fa4f43f1f5913f3ea8f782d
Hinoronori Nakano <noribasket0429200001@yahoo.co.jp>
2020-11-17 01:28:14 +0900

    写真およびボタンの実装

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Todo from './todo.js'
+import Icon from './mypicture.jpg'
+import BackGround from './background.mp4'
+import './index.css'
 
-// class MainPage extends React.Component {
-	// render(){
-		// return(
-			// <div className='MainPage'>
-				// <h1>Hironori Nakano</h1>
-				// <p>Yahoo Enginner</p>
-			// </div>
-		// )
-	// }
-// }
+class MainPage extends React.Component {
+	render(){
+		return(
+			<html>
+				<head>
+					<title>nakano</title>
+				</head>
+				<body>
+					<div className='MainPage'> <h1>Hironori Nakano</h1>
+						<div className='mypicture'>
+							<img src={Icon}/>
+						</div>
+						<div className='list'>
+								<a href="#" className="btn-sf-like">Carrer</a>
+								<a href="#" className="btn-sf-like">Research</a>
+						</div>
+					</div>
+				</body>
+			</html>
+		)
+	}
+}
 
-// ReactDOM.render(<MainPage />,document.getElementById('root'))
-ReactDOM.render(<Todo />, document.getElementById('root'));
+ReactDOM.render(<MainPage />,document.getElementById('root'))
+// ReactDOM.render(<Todo />, document.getElementById('root'));
