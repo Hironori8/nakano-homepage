b10d8e019ce0015c5c879179291f6d143183d68e
Hinoronori Nakano <noribasket0429200001@yahoo.co.jp>
2020-11-17 02:31:44 +0900

    #1 CHANGE 経歴、研究の項目の適切な配置

--- src/index.js
+++ src/index.js
@@ -8,22 +8,18 @@ import './index.css'
 class MainPage extends React.Component {
 	render(){
 		return(
-			<html>
-				<head>
-					<title>nakano</title>
-				</head>
-				<body>
-					<div className='MainPage'> <h1>Hironori Nakano</h1>
-						<div className='mypicture'>
-							<img src={Icon}/>
-						</div>
-						<div className='list'>
-								<a href="#" className="btn-sf-like">Carrer</a>
-								<a href="#" className="btn-sf-like">Research</a>
-						</div>
+			<div className='MainPage'> 
+				<h1>Hironori Nakano</h1>
+				<div className='wrapper'>
+					<div className='mypicture'>
+						<img src={Icon}/>
 					</div>
-				</body>
-			</html>
+					<div className='list'>
+						<a href="#" className="btn-sf-like">Carrer</a>
+						<a href="#" className="btn-sf-like">Research</a>
+					</div>
+				</div>
+			</div>
 		)
 	}
 }
