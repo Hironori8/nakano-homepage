f02d8339df62a0c6eed5aa36f87f98ff8742ccac
Hinoronori Nakano <noribasket0429200001@yahoo.co.jp>
2020-11-20 02:51:06 +0900

    #6 CHANGE React仕様にリファクタリング

--- src/index.js
+++ src/index.js
@@ -6,27 +6,53 @@ import GitHubButton from 'react-github-btn'
 import ReactTwitterFollowButton from 'react-twitter-follow-button';
 import InstagramButton from  "react-instagram-button";
 
+function LinkButton(){
+	return(
+		<div className='link-list'>
+			<div className='link-list-0'>
+				<GitHubButton className='github-button' 
+					href="https://github.com/Hironori8" 
+					data-size="large" 
+					aria-label="Follow @Hironori8 on GitHub">
+					Follow @Hironori8
+				</GitHubButton>
+
+			</div>
+			<div className='link-list-0'>
+				<ReactTwitterFollowButton 
+					twitterAccount='nori_nori_8' 
+					showLarge={true} 
+					showName={true} 
+					showCount={false} />
+			</div>
+			<div className='link-list-1'>
+				<InstagramButton 
+					username={"noriduckbasket12"} />
+			</div>
+		</ div>
+
+	);
+}
+
+function Picture(){
+	return(
+		<div className='mypicture'>
+			<img src={Icon}/>
+		</div>
+	);
+}
+
 class MainPage extends React.Component {
 	render(){
 		return(
 			<div className='MainPage'> 
 				<h1>Hironori Nakano</h1>
 				<div className='wrapper'>
-					<div className='mypicture'>
-						<img src={Icon}/>
-					</div>
+					<Picture/>
 					<div className='list'>
 						<a href="#" className="btn-sf-like">Carrer</a>
 						<a href="#" className="btn-sf-like">Research</a>
-						<div className='link-list-0'>
-						<GitHubButton className='github-button' href="https://github.com/Hironori8" data-size="large" aria-label="Follow @Hironori8 on GitHub">Follow @Hironori8</GitHubButton>
-						</div>
-						<div className='link-list-1'>
-						<ReactTwitterFollowButton twitterAccount='nori_nori_8' showLarge={true} showName={true} showCount={false} />
-						</div>
-						<div className='link-list-2'>
-						<InstagramButton username={"noriduckbasket12"} />
-						</div>
+						<LinkButton />
 					</div>
 				</div>
 			</div>
@@ -35,4 +61,3 @@ class MainPage extends React.Component {
 }
 
 ReactDOM.render(<MainPage />,document.getElementById('root'))
-// ReactDOM.render(<Todo />, document.getElementById('root'));
