d691cb91a2eea7bc37c150b885527ff0dced4566
Hinoronori Nakano <noribasket0429200001@yahoo.co.jp>
2021-02-18 02:14:45 +0900

    Refactor some components and Fix Carrer page.

--- src/Component/Avatar.js
+++ src/Component/Avatar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
-import Image from './mypicture.jpg';
+import Image from '../mypicture.jpg';
 
 const useStyles = makeStyles((theme) => ({
   root: {
